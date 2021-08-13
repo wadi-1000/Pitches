05d8f8287732ac67c1bcfad0da320d9063a7d758
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-08-13 19:12:40 +0300

    Added bootstrap a for better look

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,22 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+
+
+bootstrap = Bootstrap()
 
 # Initializing app
 app = Flask(__name__)
  
- def create_app():
+def create_app(config_name):
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
 
-# Registering the blueprint
-from .main import main as main blueprint
-app.register_blueprint(main_blueptint)
-
-
-return app
+    return app
