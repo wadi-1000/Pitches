b5f4391d71240aecbc8a39e16e4460186d80e0af
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-08-23 11:42:45 +0300

    Final push of project

--- app/main/views.py
+++ app/main/views.py
@@ -17,14 +17,7 @@ def index():
     elevator = Pitch.query.filter_by(category = 'Elevator').all()
     product = Pitch.query.filter_by(category = 'Product').all()
     return render_template('index.html', interview = interview,elevator = elevator, pitches = pitches,product= product)
-@main.route('/user/<uname>')
-def profile(uname):
-    user = User.query.filter_by(username = uname).first()
 
-    if user is None:
-        abort(404)
-
-    return render_template("profile/profile.html", user = user)
 
 
 @main.route('/user/<uname>/update',methods = ['GET','POST'])
@@ -46,16 +39,6 @@ def update_profile(uname):
 
     return render_template('profile/update.html',form =form)
 
-@main.route('/user/<uname>/update/pic',methods= ['POST'])
-@login_required
-def update_pic(uname):
-    user = User.query.filter_by(username = uname).first()
-    if 'photo' in request.files:
-        filename = photos.save(request.files['photo'])
-        path = f'photos/{filename}'
-        user.profile_pic_path = path
-        db.session.commit()
-    return redirect(url_for('main.profile',uname=uname))
 
 @main.route('/create_new', methods = ['POST','GET'])
 @login_required
@@ -154,35 +137,3 @@ def dislike(id):
     new_downvote.save()
     return redirect(url_for('main.index',id = id))
 
-
-@main.route('/like/<int:id>',methods = ['POST','GET'])
-@login_required
-def like(id):
-    get_pitches = Upvote.get_upvotes(id)
-    valid_string = f'{current_user.id}:{id}'
-    for pitch in get_pitches:
-        to_str = f'{pitch}'
-        print(valid_string+" "+to_str)
-        if valid_string == to_str:
-            return redirect(url_for('main.index',id=id))
-        else:
-            continue
-    new_vote = Upvote(user = current_user, pitch_id=id)
-    new_vote.save()
-    return redirect(url_for('main.index',id=id))
-
-@main.route('/dislike/<int:id>',methods = ['POST','GET'])
-@login_required
-def dislike(id):
-    pitch = Downvote.get_downvotes(id)
-    valid_string = f'{current_user.id}:{id}'
-    for p in pitch:
-        to_str = f'{p}'
-        print(valid_string+" "+to_str)
-        if valid_string == to_str:
-            return redirect(url_for('main.index',id=id))
-        else:
-            continue
-    new_downvote = Downvote(user = current_user, pitch_id=id)
-    new_downvote.save()
-    return redirect(url_for('main.index',id = id))
